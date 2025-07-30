8e0f00f2053a6d05ea7dd169bb34f8b354d3915b
YashNadeeshan <yashithanadeeshan3@gmail.com>
2025-07-30 21:07:11 +0530

    updated mysql.java

--- src/lk/supramart/connection/MySQL.java
+++ src/lk/supramart/connection/MySQL.java
@@ -1,40 +1,86 @@
 package lk.supramart.connection;
 
-import java.sql.DriverManager;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.util.Properties;
+import lk.supramart.util.LoggerUtil;
 
 public class MySQL {
 
-    private static final String DATABASE = "supramart";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "ENTER YOUR PASSWORD";
+    private static Properties appProperties;
     private static Connection connection;
-    
+
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DATABASE, USERNAME, PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
+            appProperties = new Properties();
+            String filePath = System.getProperty("user.dir") + "\\config.properties";
+
+            try (java.io.InputStream fis = new java.io.FileInputStream(filePath);) {
+                appProperties.load(fis);
+            } catch (FileNotFoundException e) {
+                LoggerUtil.Log.severe(MySQL.class, "File Not Found Exception :" + e.getMessage());
+            } catch (IOException e) {
+                LoggerUtil.Log.severe(MySQL.class, "IO Exception :" + e.getMessage());
+            }
+        } catch (ClassNotFoundException e) {
+            LoggerUtil.Log.severe(MySQL.class, "Class Not Found Exception :" + e.getMessage());
         }
+
     }
-    
+
+    private static final String DB_URL = appProperties.getProperty("db.url");
+    private static final String DB_USERNAME = appProperties.getProperty("db.username");
+    private static final String DB_PASSWORD = appProperties.getProperty("db.password");
+
     public static Connection getConnection() {
+
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            }
+        } catch (SQLException e) {
+            LoggerUtil.Log.severe(MySQL.class, "SQL Exception :" + e.getMessage());
+        }
         return connection;
     }
     
-    public static ResultSet executeSearch(String query) throws SQLException {
-        return connection.createStatement().executeQuery(query);
+    public static ResultSet executePreparedSearch(String query, Object... params) throws SQLException {
+        PreparedStatement ps = prepareStatement(query, params);
+        return ps.executeQuery();
     }
 
-    public static void executeIUD(String query) {
+    public static int executePreparedIUD(String query, Object... params) throws SQLException {
+        PreparedStatement ps = prepareStatement(query, params);
+        return ps.executeUpdate();
+    }
+
+    private static PreparedStatement prepareStatement(String query, Object... params) throws SQLException {
+        PreparedStatement ps = getConnection().prepareStatement(query);
+        for (int i = 0; i < params.length; i++) {
+            ps.setObject(i + 1, params[i]);
+        }
+        return ps;
+    }
+
+    public static void closeConnection() {
         try {
-            connection.createStatement().executeUpdate(query);
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LoggerUtil.Log.severe(MySQL.class, "SQL Exception :" + e.getMessage());
         }
     }
+
+    public static Properties getAppProperties() {
+        return appProperties;
+    }
+    
 }
+
