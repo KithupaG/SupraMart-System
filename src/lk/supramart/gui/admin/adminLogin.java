69adcee1ffb725d84e294c01d9a74993d21afc82
KithupaG <gkithupa@gmail.com>
2025-07-31 02:07:27 +0530

    added ui upgrades to admin login

--- src/lk/supramart/gui/admin/adminLogin.java
+++ src/lk/supramart/gui/admin/adminLogin.java
@@ -4,6 +4,8 @@
  */
 package lk.supramart.gui.admin;
 
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 /**
  *
  * @author kithu
@@ -16,6 +18,7 @@ public class adminLogin extends javax.swing.JFrame {
      * Creates new form adminLogin
      */
     public adminLogin() {
+        setUndecorated(true);
         initComponents();
     }
 
@@ -120,6 +123,7 @@ public class adminLogin extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
@@ -134,24 +138,7 @@ public class adminLogin extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
-            logger.log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+        FlatMacLightLaf.setup();
         java.awt.EventQueue.invokeLater(() -> new adminLogin().setVisible(true));
     }
 
