0792cc5c8e98039a58ba632217cdab801287f390
YashNadeeshan <yashithanadeeshan3@gmail.com>
2025-08-07 00:51:48 +0530

    resolved

---
+++ src/lk/supramart/dao/ProductDAO.java
@@ -0,0 +1,17 @@
+package lk.supramart.dao;
+
+import java.util.List;
+
+public interface ProductDAO {
+
+    void addProduct(Product product);
+
+    Product getProductById(int id);
+
+    List<Product> getAllProducts();
+
+    void updateProduct(Product product);
+
+    void deleteProduct(int id);
+
+}
