08775ffd86629a8e9b723e79537162b71bf07364
SandySew <sandunisewwandi421@gmail.com>
2025-08-08 23:09:23 +0530

    Update Discount.java

--- src/lk/supramart/model/Discount.java
+++ src/lk/supramart/model/Discount.java
@@ -1,38 +1,73 @@
 package lk.supramart.model;
 
 import java.time.LocalDate;
-import lk.supramart.enums.DiscountType;
 
 /**
  *
  * @author Sanduni PC
  */
 public class Discount {
-    private int id;
-    private String name;
-    private DiscountType type;
-    private double value;
+    private int discountId;
+    private String discountName;
+    private double discountPercentage;
     private LocalDate startDate;
     private LocalDate endDate;
-    private boolean active;
     
-    public Discount(int id, String name, DiscountType type, double value, LocalDate startDate, LocalDate endDate, boolean active){
-        this.id = id;
-        this.name = name;
-        this.type = type;
+    public Discount(){
+    
+    }
+    
+    public Discount(int discountId, String discountName, double discountPercentage, LocalDate startDate, LocalDate endDate){
+        this.discountId = discountId;
+        this.discountName = discountName;
+        this.discountPercentage = discountPercentage;
         this.startDate = startDate;
         this.endDate = endDate;
-        this.active = active;
     }
     
     // Getters and Setters
     // toString(), equals(), hashCode() as needed
 
-    public int getId() {
-        return id;
+    public int getDiscountId(){
+        return discountId;
     }
-
-    public boolean isActive() {
-        return active;
+    
+    public void setDiscountId(int DiscountId){
+        this.discountId = discountId;
     }
+    
+    public String getDiscountName(){
+        return discountName;
+    }
+    
+    public void setDiscountName(String DiscountName){
+        this.discountName = discountName;
+    }
+    
+    public double getDiscountPercentage(){
+        return discountPercentage;
+    }
+    
+    public void setDiscountPercentage(){
+        this.discountPercentage = discountPercentage;
+    }
+    
+    public LocalDate getStartDate(){
+        return startDate;
+    }
+    
+    public void setStartDate(){
+        this.startDate = startDate;
+    }
+    
+    public LocalDate getEndDate(){
+        return endDate;
+    }
+    
+    public void setEndDate(){
+        this.endDate = endDate;
+    }
+    
+    
 }
+
