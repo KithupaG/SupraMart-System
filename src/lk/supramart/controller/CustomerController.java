174f0bd4e2abcb826571bd0f6de586fe29965c0c
KithupaG <gkithupa@gmail.com>
2025-08-09 18:45:24 +0530

    Created Customer Component Files
    Making customer component since we have no activity from Dinan

---
+++ src/lk/supramart/controller/CustomerController.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lk.supramart.controller;
+
+/**
+ *
+ * @author kithu
+ */
+public class CustomerController {
+    
+}
